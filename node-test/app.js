1c7f31108a302ed42503ac6405ba0f97d47850ea
David Duong <david@duong.cz>
2018-06-23 21:29:44 +0200

    mock api & finish chatbot

--- node-test/app.js
+++ node-test/app.js
@@ -4,41 +4,135 @@ const bodyparser = require('body-parser')
 const unfluff = require('unfluff')
 const { JSDOM } = require('jsdom')
 const fetch = require('node-fetch')
+const { AmioApi, WebhookRouter } = require('amio-sdk-js')
+
+const chatbot = new AmioApi({
+    accessToken: 'Rp5s9KybT7Yi6RtdNbHqsIJ8Xh9BfBAKMMD5HhVLiRdFIRr34iICo65QG1rsko0pFUkMqbZYJiFtLbWLTHFbg1Wpay'
+})
+
+const chatRouter = new WebhookRouter({
+    secretToken: 'zob6f4FuFeO5I4Gl4shmHyswivEcRN'
+})
+
 
 app.use(bodyparser.json())
 
-app.post('/receiveBot', async (req, res) => {
-    try {
-        const { event, data } = req.body
-        if (event === 'message_received') {
-            const { channel: { id: channelId }, contact: { id: userId }, content: { payload, type } } = data
-            if (type === 'text') {
-                const status = await fetch('https://api.amio.io/v1/messages', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": "Bearer Rp5s9KybT7Yi6RtdNbHqsIJ8Xh9BfBAKMMD5HhVLiRdFIRr34iICo65QG1rsko0pFUkMqbZYJiFtLbWLTHFbg1Wpay"
-                    },
-                    body: JSON.stringify({
-                        "channel": { "id": channelId },
-                        "contact": { "id": userId },
-                        "content": {
-                            "type": "text",
-                            "payload": payload.toLocaleUpperCase()
-                        }
-                    })
-                }).then((data) => {
-                    return data.text()
-                })
-                res.send("Stuff is fine")
-            }
+const handleRequest = async (text) => {
+    if (text === 'server error') throw Error('server error')
+    return {
+        status: true,
+        result: {
+            validity: (text === 'valid input'),
+            sources: [
+                { source: 'Washington Post', title: 'Some title 2', link: "https://cloud.google.com/vision/docs/ocr" },
+                { source: 'Reuters', title: 'Some title', link: "https://cloud.google.com/vision/docs/ocr" }
+            ]
         }
+    }
+}
+
+
+chatRouter.onError(error => console.error(error))
+chatRouter.onMessageReceived(async (data, timestamp) => {
+    console.log('new Message received', data)
+
+    if (data.channel && data.contact && data.content) {
+        const { channel: { id: channelId }, contact: { id: contactId }, content: { type, payload: text }} = data
+        
+        const result = await handleRequest(text) 
+
+        if (result.status === true && result.result) {
+            const { validity, sources } = result.result
+            await chatbot.messages.send({
+                channel: { id: channelId },
+                contact: { id: contactId },
+                content: { type: 'text', payload: validity ? `The statement, and I quote: "${text}", is most likely TRUE ✔️` : `The statement, and I quote: "${text}", is most likely FALSE ❌` }
+            })
+
+            if (sources && sources.length > 0) {
+                await chatbot.messages.send({
+                    channel: { id: channelId },
+                    contact: { id: contactId },
+                    content: { type: 'text', payload: validity ? 'Here are some confirmations:' : 'Here are some valid sources:' }
+                })
+
+                
+                await chatbot.messages.send({
+                    channel: { id: channelId },
+                    contact: { id: contactId }, 
+                    content: {
+                        type: 'structure',
+                        payload: sources.map(({ source, title, link }) => (
+                            {
+                                title: title,
+                                text: source,
+                                buttons: [
+                                    {
+                                        type: "url",
+                                        title: "Visit website",
+                                        payload: link
+                                    }
+                                ]
+                            }
+                        ))
+                    }
+                })
+            }
+
+        } else {
+            await chatbot.messages.send({
+                channel: { id: channelId },
+                contact: { id: contactId },
+                content: { type, payload: `There are some issues with our backend ... pretty bad during a pitch :(` }
+            })
+        }
+
+    }
+})
+
+
+chatRouter.onPostbackReceived(async (data, timestamp) => {
+    if (data.channel && data.contact) {
+        const { channel: { id: channelId }, contact: { id: contactId } } = data
+
+        await chatbot.messages.send({
+            channel: { id: channelId },
+            contact: { id: contactId },
+            content: {
+                type: 'text',
+                payload: 'Fact check 🔎 anything on the go ✔️. Just type your sentence to be checked from various sources'
+            }
+        })
+
+        await chatbot.messages.send({
+            channel: { id: channelId },
+            contact: { id: contactId },
+            content: {
+                type: 'text',
+                payload: 'Just type your sentence to be checked from various sources'
+            }
+        })
+    }
+})
+
+
+app.post('/factcheck', async(req,res) => {
+    try {
+        const text = req.body.text
+        const result = await handleRequest(text)
+        res.status(200)
+        res.send(JSON.stringify(result))
     } catch (err) {
         console.log(err)
         res.status(500)
-        res.send("")
+        res.send(JSON.stringify({
+            status: false,
+            result: err
+        }))
     }
 })
+
+app.post('/receiveBot', (req, res) => chatRouter.handleEvent(req, res))
 
 app.post('/extract', async (req, res) => {
     try {
